884ec82dccce02eda02535f12d75de22c36e44a6
Ilya Uglovskoy <kaproman00@mail.ru>
2020-11-06 15:41:11 +0300

    feat script.js: add animation and edit

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ class Todo {
         this.todoList = document.querySelector(todoList);
         this.todoCompleted = document.querySelector(todoCompleted);
         this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
-        this.todoContainer = document.querySelector('.todo-container');
+        this.todoContainer = document.querySelector('.todo-container'),
+        this.requestID;
     }
 
 
@@ -28,11 +29,12 @@ class Todo {
     createElement(todo){
         const li = document.createElement('li');
         li.classList.add('todo-item');
-        li.dataset.key = todo.key;
         li.style.opacity = 1;
+        li.dataset.key = todo.key;
         li.insertAdjacentHTML('beforeend', `
             <span class="text-todo">${todo.value}</span>
             <div class="todo-buttons">
+                <button class="todo-edit"></button>
                 <button class="todo-remove"></button>
                 <button class="todo-complete"></button>
             </div>
@@ -54,6 +56,18 @@ class Todo {
     }
 
 
+    animate(target, cb) {
+        let value = parseFloat(target.style.opacity);
+        this.requestID = setInterval(() => {
+            if(value <= 0){
+                clearInterval(this.requestID);
+                cb.call(this, target);
+            }
+            value -= 0.1;
+            target.style.opacity = value;
+        }, 50);
+    }
+
     deleteItem(target){
         this.todoData.forEach((item) => {
             if(item.key === target.dataset.key){
@@ -65,11 +79,25 @@ class Todo {
     }
 
     completedItem(target){
-        this.todoData.forEach((item, i) => {
+        this.todoData.forEach((item) => {
             if(item.key === target.dataset.key){
                 this.todoData.set(item.key, {
                     'value': item.value,
                     'completed': !item.completed,
+                    'key': item.key
+                });
+                this.addToStorage();
+                this.render();
+            }
+        });
+    }
+
+    editItem(target){
+        this.todoData.forEach((item) => {
+            if(item.key === target.dataset.key){
+                this.todoData.set(item.key, {
+                    'value': target.children[0].textContent,
+                    'completed': item.completed,
                     'key': item.key
                 });
                 this.addToStorage();
@@ -84,10 +112,18 @@ class Todo {
 
             const target = event.target;
             if(target.matches('.todo-remove')){
-                this.deleteItem(target.parentNode.parentNode);
+                this.animate(target.parentNode.parentNode, this.deleteItem)
             }
             if(target.matches('.todo-complete')){
-                this.completedItem(target.parentNode.parentNode);
+                this.animate(target.parentNode.parentNode, this.completedItem);
+            }
+            if(target.matches('.todo-edit')){
+                const li = target.parentNode.parentNode;
+                li.setAttribute('contenteditable', 'true');
+
+                li.addEventListener('blur', () => {
+                    this.editItem(li);
+                });
             }
         });
 
